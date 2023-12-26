e43d1c8a5a1bd874812297d8a0c3c1f78999db3c
Felipe de Senna <lipe.senna@hotmail.com>
2023-12-26 10:58:52 +0000

    feat: project rxjs

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,52 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { EMPTY, BehaviorSubject, catchError, combineLatest, map } from 'rxjs';
+
+import { ProductService } from './product.service';
+import { ProductCategoryService } from '../product-categories/product-category.service';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductListComponent {
+  pageTitle = 'Product List';
+  errorMessage = '';
+
+  private categorySelectedSubject = new BehaviorSubject<number>(0);
+  categorySelectedAction$ = this.categorySelectedSubject.asObservable();
+
+  products$ = combineLatest([
+    this.productService.productsWithAdd$,
+    this.categorySelectedAction$,
+  ]).pipe(
+    map(([products, selectedCategoryId]) =>
+      products.filter(product => selectedCategoryId ? product.categoryId === selectedCategoryId : true)
+    ),
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
+
+  categories$ = this.productCategoryService.productCategories$.pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
+
+  constructor(
+    private productService: ProductService,
+    private productCategoryService: ProductCategoryService,
+  ) { }
+
+  onAdd(): void {
+    this.productService.addProduct();
+  }
+
+  onSelected(categoryId: string): void {
+    this.categorySelectedSubject.next(+categoryId);
+  }
+}
